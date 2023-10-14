f7aa3f8a57f851ac2461ebb7d9639e8f9d2076c9
a.hristolubov@gmail.com <61623449+spacemonkey333f@users.noreply.github.com>
2023-10-14 10:19:05 +0300

    1

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val searchButton = findViewById<Button>(R.id.button_search)
+        val mediaLibButton = findViewById<Button>(R.id.button_medialib)
+        val settingsButton = findViewById<Button>(R.id.button_settings)
+
+        /*searchButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "поиск", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        mediaLibButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "медиатека", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        settingsButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "настройки", Toast.LENGTH_SHORT).show()
+            }
+        })*/
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediaLibButton.setOnClickListener {
+            val mediaIntent = Intent(this, MedialibActivity::class.java)
+            startActivity(mediaIntent)
+        }
+
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+
     }
 }
